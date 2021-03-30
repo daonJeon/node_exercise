8ffc74697eab2e0f1a7fee83f1865f1bf9d23aa7
jeonsooyoung <peace2332@naver.com>
2021-03-30 13:25:52 +0900

    file create update

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-var http = require("http");
-var fs = require("fs");
-var url = require("url");
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control){
   return `
     <!doctype html>
     <html>
@@ -13,66 +14,54 @@ function templateHTML(title, list, body) {
     <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
     </body>
     </html>
     `;
 }
 
-function templateLIST(fileList) {
-  var list = "<ul>";
-  for (let list_i = 0; list_i < fileList.length; list_i++) {
-    const fileName = fileList[list_i].toUpperCase();
-    list += `<li><a href="?id=${fileName}">${fileName}</a></li>`;
+function templateLIST(filelist) {
+  var list = '<ul>';
+  for (var list_i = 0; list_i < filelist.length; list_i++) {    
+    list += `<li><a href="/?id=${filelist[list_i]}">${filelist[list_i]}</a></li>`;
   }
-  list += "</ul>";
+  list += '</ul>';
   return list;
 }
 
-var app = http.createServer(function (request, response) {
+var app = http.createServer(function(request,response){
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-
-  if (pathname === "/") {
-      console.log(pathname)
-    if (queryData.id === undefined) {
-      fs.readdir("./data", function (err, fileList) {
-        var title = "Welcome";
-        var description = "hello Node.js";
-        var list = templateLIST(fileList);
-
-        var template = templateHTML(
-          title,
-          list,
-          `<h2>${title}</h2>${description}`
-        );
+  if(pathname === '/'){
+    if(queryData.id === undefined){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = templateLIST(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,`<a href="/create">create</a>`);
         response.writeHead(200);
         response.end(template);
       });
     } else {
-        fs.readdir("./data", function (err, fileList) {
-            fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
+        fs.readdir('./data', function(error, filelist) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 var title = queryData.id;
-                var list = templateLIST(fileList);
-    
-                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                var list = templateLIST(filelist);
+                var template = templateHTML(title, list, `
+                <h2>${title}</h2>${description}`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
                 response.writeHead(200);
                 response.end(template);
             });
-        });
-     
+        });     
     }
   } else if(pathname === '/create'){
-    fs.readdir("./data", function (error, filelist) {
-      var title = "WEB - create";
-      var list = templateList(filelist);
-      var template = templateHTML(
-        title,
-        list,
-        `
-          <form action="http://localhost:3000/process_create" method="post">
+    fs.readdir('./data', function(error, filelist){
+      var title = 'WEB - create';
+      var list = templateLIST(filelist);//이 부분 오타 매서드명 오타 
+      var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -81,14 +70,32 @@ var app = http.createServer(function (request, response) {
               <input type="submit">
             </p>
           </form>
-        `
-      );
-      response.writeHead(200);
+        `,``);
+      response.writeHead(200);//성공
       response.end(template);
     });
+  } else if(pathname === '/create_process'){//Post 방식으로 보낸 데이터를 Node js에서 불러오기!!
+    var body = '';
+    request.on('data',function(data){//
+        body += data;
+    })//node js로 접속 들어올때마다 콜백 함수로 node 호출
+    request.on('end',function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description,'utf8',
+        function(err){
+          response.writeHead(302, {Location:`/?id=${title}`});
+          response.end();
+        })      
+    })
+
   } else {
     response.writeHead(404);
-    response.end("Not found");
+    response.end('Not found');
   }
+
+
+
 });
 app.listen(3000);
