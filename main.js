daf4e929bd405dba475a5813624aa65a2b19ccee
jeonsooyoung <peace2332@naver.com>
2021-04-01 12:55:22 +0900

    delete function add

--- main.js
+++ main.js
@@ -50,7 +50,16 @@ var app = http.createServer(function(request,response){
                 var title = queryData.id;
                 var list = templateLIST(filelist);
                 var template = templateHTML(title, list, `
-                <h2>${title}</h2>${description}`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                <h2>${title}</h2>${description}`,
+                `
+                  <a href="/create">create</a> 
+                  <a href="/update?id=${title}">update</a> 
+                  <form action="delete_process" method="post" onsubmit="">
+                    <input type="hidden" name="id" value="${title}">
+                    <input type="submit" value="delete">
+                  </form>
+
+                `);
                 response.writeHead(200);
                 response.end(template);
             });
@@ -138,6 +147,23 @@ var app = http.createServer(function(request,response){
           response.writeHead(302, {Location:`/?id=${title}`});
           response.end();
         })     
+      })
+    })
+  } else if(pathname === '/delete_process'){ 
+    var body = '';
+    request.on('data',function(data){
+      body += data;
+    })
+
+    request.on("end",function(){
+      var post = qs.parse(body)      
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+
+      fs.unlink(`data/${id}`,function(error){
+        response.writeHead(302, {Location:`/?id=${title}`});
+        response.end();   
       })
     })
   } else {
